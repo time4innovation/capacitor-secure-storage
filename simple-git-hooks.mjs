bed49e51801b44a54d8444706ee4d3bbf85ac48b
Oliver Schmidt <oliver.schmidt@time4innovation.de>
2025-10-06 15:36:18 +0200

    fixes for publishing and dist folder

--- simple-git-hooks.mjs
+++ simple-git-hooks.mjs
@@ -4,8 +4,8 @@
   Be sure to run `simple-git-hooks` after modifying this file.
 */
 const exports = {
-  'commit-msg': 'cat "$@" | "$PWD"/node_modules/.bin/commitlint',
-  'pre-commit': 'pnpm lint',
+  //   'commit-msg': 'cat "$@" | "$PWD"/node_modules/.bin/commitlint',
+  //   'pre-commit': 'pnpm lint',
 }
 
 export default exports
